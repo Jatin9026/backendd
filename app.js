f5335a3a222d97d81b0faab5f0df0e0a9fba146a
Jatin Gupta <jatingupta918306@gmail.com>
2025-12-01 21:27:22 +0530

    Add new allowed origin for CORS

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ import cors from 'cors';
 dotenv.config({ path: './config/config.env' });
 const app = express();
 
-const allowedOrigins = ["http://localhost:4000", "http://localhost:3000", "http://localhost:8000", "http://localhost:5000", "https://mern-backend-t3h8.onrender.com"];
+const allowedOrigins = ["http://localhost:4000", "http://localhost:3000", "http://localhost:8000", "http://localhost:5000", "https://mern-backend-t3h8.onrender.com","https://backend-a7o8.onrender.com"];
 app.use(cors({
     origin: (origin, callback) => {
         // allow requests with no origin (like mobile apps or curl requests)
@@ -82,5 +82,6 @@ app.get("/", (req, res) => {
 
 // Error middleware
 app.use(errorHandleMiddleware);
+
 
 export default app;
